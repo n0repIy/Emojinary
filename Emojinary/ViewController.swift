f179ef7a643c747e5d7ea9d0ec0f2cb7ca945bd3
Wade Winright <wawinrig@macbook-pro.local>
2018-01-08 22:25:21 +0000

    Initial Commit

---
+++ Emojinary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emojinary
+//
+//  Created by Wade Winright on 08/01/2018.
+//  Copyright © 2018 Fuquov Jackazh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
