75b3a5b579e3b98ac21115f9608819c456049dda
Wade Winright <wawinrig@macbook-pro.local>
2018-01-10 11:49:12 +0000

    Added second screen and tableview

--- Emojinary/ViewController.swift
+++ Emojinary/ViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tblview: UITableView!
+    
+    var emojizz = ["🖕","😁","😕","🙁","🤬","💩","👾","🤷‍♀️"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tblview.dataSource = self
+        tblview.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojizz.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojizz[indexPath.row]
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
